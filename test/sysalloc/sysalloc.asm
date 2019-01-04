9bec460e82bee8e26e89a012e01a41429eb59673
Orlando Llanes <orlandol@veribox.net>
2019-01-04 03:38:21 -0500

    Added the start of the system memory allocator test.

---
+++ test/sysalloc/sysalloc.asm
@@ -0,0 +1,25 @@
+
+  CPU 386
+
+  BITS 32
+
+segment .text use32
+
+  %include "sys.inc"
+  %include "rtl.inc"
+
+..start:
+  push    ebp
+  mov     ebp, esp
+
+  call    InitRTL
+
+  mov     esp, ebp
+  pop     ebp
+
+  push    dword 0
+  call    [ExitProcess]
+
+segment .data use32
+
+section .bss use32
