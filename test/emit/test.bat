d39a2119d4f8bd8bd1c2a75ade9a357fe119fa9b
Orlando Llanes <orlandol@veribox.net>
2019-02-11 02:06:26 -0500

    Renamed emitcg to emit. Renamed instrcg to codegen. Memory parameter encoding in progress.

--- test/emit/test.bat
+++ test/emit/test.bat
@@ -1,5 +1,5 @@
 @echo off
-emitcg
+emit
 fc /b /LB0 test.bin expected.bin
 if %errorlevel% == 0 echo Test passed
 if not %errorlevel% == 0 echo Test FAILED
--- test/emitcg/expected.asm
+++
@@ -1,6 +0,0 @@
-
-CPU 386
-
-BITS 32
-
-mov dword [fs:ebx + ecx * 8 + 0x12345678], 0x89ABCDEF
