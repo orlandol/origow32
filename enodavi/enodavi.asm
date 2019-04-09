ff7843268c2f95d3e9ed475d1781e42ebe5e5f04
Orlando Llanes <orlandol@veribox.net>
2019-04-09 01:41:26 -0400

    String code in progress.

--- enodavi/enodavi.asm
+++ enodavi/enodavi.asm
@@ -7,6 +7,7 @@ segment .text use32
 
   %include "rtl.inc"
   %include "console.inc"
+  %include "ui.inc"
 
 extern GetLastError
 import GetLastError kernel32.dll
@@ -17,6 +18,18 @@ run:
   mov     ebp, esp
 
   call    InitRTL
+
+  call    InitUI
+
+  ; BOOL WINAPI WriteConsoleA( HANDLE  hConsoleOutput,
+  ;   const VOID *lpBuffer, DWORD nNumberOfCharsToWrite,
+  ;   LPDWORD lpNumberOfCharsWritten, LPVOID lpReserved )
+  push    dword 0
+  push    dword 0
+  push    dword [arrowStr - 4]
+  push    arrowStr
+  push    dword [stdOut]
+  call    [WriteConsoleA]
 
   push    dword TRUE
   push    interceptCtrl
@@ -51,6 +64,8 @@ interceptCtrl:
   ret 4
 
 segment .data use32
+
+  declstring arrowStr, 16, 17, 30, 31
 
   inputBuffer: times (128 * SIZEOF_INPUT_RECORD) db 0
 
